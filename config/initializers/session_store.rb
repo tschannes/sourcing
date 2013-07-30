ffb9b83388b93038c0987d4790b9947deea01547
tschannes <mail@tscherrig.net>
2013-07-30 14:59:55 -0400

    reset

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Sources::Application.config.session_store :cookie_store, key: '_sources_session'
--- evernote_oauth/lib/evernote_oauth/version.rb
+++
@@ -1,3 +0,0 @@
-module EvernoteOauth
-  VERSION = "0.0.1"
-end

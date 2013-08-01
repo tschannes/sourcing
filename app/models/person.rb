f821baccfe19048fe673fa98645b6d26bff005f4
tschannes <mail@tscherrig.net>
2013-08-01 17:07:18 -0400

    person model tests added

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,8 @@ class Person < ActiveRecord::Base
 	has_many :tags, as: :taggable
 	has_many :taggables, :through => :tags, :source => :taggable, :source_type => "Person"
 	belongs_to :category, foreign_key: "cat_id"
+
+	validates :description, :rating, :cat_id, :user_id, :first_name, :last_name, :nick_name, :email, :job, presence: true
+	validates_format_of :email, :with => /@/
+
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,10 +1,21 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  test "that person data is valid" do 
+ 
+  test "that person data is not empty" do
+    array = [:description, :rating, :cat_id, :user_id, :first_name, :last_name, :nick_name, :email, :job]
 
+    for i in 0..(array.length-1) do
+      person = FactoryGirl.build(:person, array[i] => "")
+      assert !person.save, "it saved an empty string to db"
+    end
   end
+
+  test "user email is valid" do
+		person = FactoryGirl.build(:person, email: "somerandomstring")
+		assert !person.save, "it saved an empty string to db"
+
+		person2 = FactoryGirl.build(:person)
+		assert person2.save, "it saved valid email to to db"
+	end
 end

60fe1c43c0d5cebc0cab5494503837cbf0b0158a
tschannes <mail@tscherrig.net>
2013-08-01 16:55:21 -0400

    user tests passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+
+	validates :first_name, :last_name, :nick_name, :email, :job, presence: true
+	validates_format_of :email, :with => /@/
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  test "user data is valid" do
-  end
-  
+
+	test "that user data is valid" do
+		array = [:first_name, :last_name, :nick_name, :email, :job]
+
+		for i in 0..(array.length-1) do
+			user = FactoryGirl.build(:user, array[i] => "")
+			assert !user.save, "it saved an empty string to db"
+		end
+	end
+
+	test "user email is valid" do
+		user = FactoryGirl.build(:user, email: "somerandomstring")
+		assert !user.save, "it saved an empty string to db"
+
+		user = FactoryGirl.build(:user, email: "somerandom@string")
+		assert user.save, "it saved valid email to to db"
+	end
+
 end
